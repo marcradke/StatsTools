2ae88217ce2497a6e2647fdb9d7ab4e6b3996251
Marc Radke <mrradke@gmail.com>
2021-03-16 21:50:23 -0700

    Update test-my_t.R - Added tests for other values of "alternative"

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -1,10 +1,11 @@
 set.seed(111)
 x <- rnorm(10, mean = 0, sd = 1)
-my_t.test(x, mu = 0)
-my_t.test(x, alternative = "less", mu = 1)
 
 test_that("output is list", {
   expect_type(my_t.test(x, mu = 0),"list")
+  expect_type(my_t.test(x, alternative = "less", mu = 0),"list")
+  expect_type(my_t.test(x, alternative = "greater", mu = 0),"list")
+
 })
 
 test_that("incorrect value of alternative throws an error", {
